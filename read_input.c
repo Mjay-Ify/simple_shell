72c0cf8888940b4ebd865a3b7cef53068b0d8b5d
Officialdeemmy <adenijiademolaafis@gmail.com>
2023-11-11 02:00:55 -0800

    function that reads lines

---
+++ read_input.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * read_input - function to reads the inserted string.
+ * @n: return value for getting the line function
+ * Return: insert string
+ */
+char *read_input(int *n)
+{
+	char *insert = NULL;
+	size_t buffersize = 0;
+
+	*n = getline(&insert, &buffersize, stdin);
+
+	return (insert);
+}

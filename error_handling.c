8ac22a90f2c47c69da7288d3e9bcf7331dfcc7f1
Officialdeemmy <139757003+Officialdeemmy@users.noreply.github.com>
2023-11-15 00:03:54 +0100

    Update error_handling.c

--- error_handling.c
+++ error_handling.c
@@ -12,44 +12,59 @@
  */
 char *strcat_custom(data_container *data, char *message, char *err, char *ver)
 {
-	char *error_flag = (char *)malloc(3);
+	char *error_flag;
 
-	custom_strcpy(err, data->av[0]);
-	custom_strcat(err, ": ");
-	custom_strcat(err, ver);
-	custom_strcat(err, ": ");
-	custom_strcat(err, data->args[0]);
-	custom_strcat(err, message);
-	error_flag[0] = '-';
-	error_flag[1] = (data->args[1][0] == '-') ? data->args[1][1] : '\0';
-	error_flag[2] = '\0';
-	custom_strcat(err, error_flag);
+	_strcpy(err, data->argv[0]);
+	_strcat(err, ": ");
+	_strcat(err, ver);
+	_strcat(err, ": ");
+	_strcat(err, data->args[0]);
+	_strcat(err, message);
+
+	if (data->args[1][0] == '-')
+	{
+		error_flag = malloc(3);
+		error_flag[0] = '-';
+		error_flag[1] = data->args[1][1];
+		error_flag[2] = '\0';
+	}
+	_strcat(err, error_flag);
 	free(error_flag);
-	custom_strcat(err, "\n");
-	custom_strcat(err, "\0");
+	else
+		_strcat(err, data->args[1]);
+
+	_strcat(err, "\n");
+	_strcat(err, "\0");
 	return (err);
 }
 
 /**
  * fetch_error_custom - error message
  * @data: relevant data or file.
- * Return: Inputs that are mesaages.
+ * Return: Inputs that are messages.
  */
 char *fetch_error_custom(data_container *data)
 {
 	int len, id;
 	char *err, *ver, *message;
 
-	ver = aux_itoa(data->counter);
-	message = (data->args[1][0] == '-') ?
-		": error option " :
+	ver = aux_itoa(data->count);
+	if (data->args[1][0] == '-')
+	{
+		": Illegal option " :
+		id = 2;
+	}
+	else
+	{
 		": can't cd to ";
-	id = (data->args[1][0] == '-') ? 2 : custom_strlen(data->args[1]);
+		id = _strlen(data->args[1]);
+	}
 
-	len = custom_strlen(data->av[0]) + custom_strlen(data->args[0])
-	+ custom_strlen(ver) + custom_strlen(message) + id + 5;
-	err = (char *)malloc(sizeof(char) * (len + 1));
-	if (err == NULL)
+	len = _strlen(data->argv[0]) + _strlen(data->args[0])
+	len += _strlen(ver) + _strlen(message) + id + 5;
+	err = malloc(sizeof(char) * (len + 1));
+	
+	if (err == 0)
 	{
 		free(ver);
 		return (NULL);
@@ -62,18 +77,18 @@ char *fetch_error_custom(data_container *data)
 }
 
 /**
- * missing_file_error - Error mssg for commands missing.
+ * missing_file_error - Error msg for commands missing.
  * @data: Important file
- * Return: Messages that shows error
+ * Return: Messages that show error
  */
 char *missing_file_error(data_container *data)
 {
 	int len;
 	char *err, *ver;
 
-	ver = aux_itoa(data->counter);
-	len = custom_strlen(data->av[0]) + custom_strlen(ver);
-	len += custom_strlen(data->args[0]) + 16;
+	ver = aux_itoa(data->count);
+	len = _strlen(data->argv[0]) + _strlen(ver);
+	len += _strlen(data->args[0]) + 16;
 	err = malloc(sizeof(char) * (len + 1));
 	if (err == 0)
 	{
@@ -81,13 +96,13 @@ char *missing_file_error(data_container *data)
 		free(ver);
 		return (NULL);
 	}
-	custom_strcpy(err, data->av[0]);
-	custom_strcat(err, ": ");
-	custom_strcat(err, ver);
-	custom_strcat(err, ": ");
-	custom_strcat(err, data->args[0]);
-	custom_strcat(err, ": not found\n");
-	custom_strcat(err, "\0");
+	_strcpy(err, data->argv[0]);
+	_strcat(err, ": ");
+	_strcat(err, ver);
+	_strcat(err, ": ");
+	_strcat(err, data->args[0]);
+	_strcat(err, ": not found\n");
+	_strcat(err, "\0");
 	free(ver);
 	return (err);
 }
@@ -103,23 +118,23 @@ char *shell_terminate_error(data_container *data)
 	char *err;
 	char *ver;
 
-	ver = aux_itoa(data->counter);
-	len = custom_strlen(data->av[0]) + custom_strlen(ver);
-	len += custom_strlen(data->args[0]) + custom_strlen(data->args[1]) + 23;
+	ver = aux_itoa(data->count);
+	len = _strlen(data->argv[0]) + _strlen(ver);
+	len += _strlen(data->args[0]) + _strlen(data->args[1]) + 23;
 	err = malloc(sizeof(char) * (len + 1));
 	if (err == 0)
 	{
 		free(ver);
 		return (NULL);
 	}
-	custom_strcpy(err, data->av[0]);
-	custom_strcat(err, ": ");
-	custom_strcat(err, ver);
-	custom_strcat(err, ": ");
-	custom_strcat(err, data->args[0]);
-	custom_strcat(err, ": error number:");
-	custom_strcat(err, data->args[1]);
-	custom_strcat(err, "\n\0");
+	_strcpy(err, data->argv[0]);
+	_strcat(err, ": ");
+	_strcat(err, ver);
+	_strcat(err, ": ");
+	_strcat(err, data->args[0]);
+	_strcat(err, ": error number:");
+	_strcat(err, data->args[1]);
+	_strcat(err, "\n\0");
 	free(ver);
 
 	return (err);

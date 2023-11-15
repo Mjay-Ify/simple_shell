bf9396beab88ff14d3cbc4eb5f9ec45234a04115
Officialdeemmy <139757003+Officialdeemmy@users.noreply.github.com>
2023-11-15 13:11:53 +0100

    Update error_handling1.c

--- error_handling1.c
+++ error_handling1.c
@@ -13,9 +13,9 @@ char *system_environment_error(data_container *data)
 	int len;
 	char *err, *ver, *message;
 
-	ver = aux_itoa(data->counter);
+	ver = intToStr(data->counter);
 	message = ": Unable to add/remove from the environment\n";
-	len = custom_strlen(data->av[0]) + custom_strlen(ver);
+	len = custom_strlen(data->argv[0]) + custom_strlen(ver);
 	len += custom_strlen(data->args[0]) + custom_strlen(message) + 4;
 	err = malloc(sizeof(char) * (len + 1));
 	if (err == 0)
@@ -57,13 +57,14 @@ char *route_126_error_alert(data_container *data)
 		free(ver);
 		return (NULL);
 	}
-	custom_strcpy(err, data->av[0]);
+	custom_strcpy(err, data->argv[0]);
 	custom_strcat(err, ": ");
 	custom_strcat(err, ver);
 	custom_strcat(err, ": ");
 	custom_strcat(err, data->args[0]);
 	custom_strcat(err, ": access revoked\n");
-
+	custom_strcat(err, "\0");
 	free(ver);
+
 	return (err);
 }
